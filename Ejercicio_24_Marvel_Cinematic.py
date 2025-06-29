b849135c82f4cd53ca2042590bfe5779a490670e
Juanceto01-jpg <juanignaciobalbuena25@gmail.com>
2025-06-29 19:40:52 -0300

    Corrección Ejercicio_24_Marvel_Cinematic.py

--- Ejercicio_24_Marvel_Cinematic.py
+++ Ejercicio_24_Marvel_Cinematic.py
@@ -1,187 +1,113 @@
-class Pila:
-    def __init__(self):
-        self.items = []
-    
-    def apilar(self, item):
-        self.items.append(item)
-    
-    def desapilar(self):
-        if not self.esta_vacia():
-            return self.items.pop()
-        return None
-    
-    def esta_vacia(self):
-        return len(self.items) == 0
-    
-    def tamano(self):
-        return len(self.items)
-    
-    def cima(self):
-        if not self.esta_vacia():
-            return self.items[-1]
-        return None
-    
-    def __str__(self):
-        return str(self.items)
+from stack_ import Stack
 
 class Personaje:
     def __init__(self, nombre, peliculas):
         self.nombre = nombre
         self.peliculas = peliculas
-    
-    def __str__(self):
-        return f"{self.nombre} ({self.peliculas} películas)"
+
+
+def mostrar_personaje(p):
+    print(p.nombre, "-", p.peliculas, "películas")
+
 
 def crear_pila_mcu():
-    pila = Pila()
+    pila = Stack()
     personajes = [
-        ("Rocket Raccoon", 6),
-        ("Groot", 5),
-        ("Iron Man", 10),
-        ("Captain America", 9),
-        ("Black Widow", 8),
-        ("Thor", 8),
-        ("Hulk", 6),
-        ("Doctor Strange", 5),
-        ("Spider-Man", 5),
-        ("Black Panther", 4),
-        ("Star-Lord", 5),
-        ("Gamora", 4),
-        ("Drax", 4),
-        ("Captain Marvel", 3),
-        ("Scarlet Witch", 5),
-        ("Vision", 4),
-        ("Falcon", 5),
-        ("Winter Soldier", 5),
-        ("War Machine", 7),
-        ("Loki", 6),
-        ("Nick Fury", 9),
-        ("Hawkeye", 5),
-        ("Ant-Man", 4),
-        ("Wasp", 3),
-        ("Doctor Strange", 5),
-        ("Star-Lord", 5),
-        ("Gamora", 4),
-        ("Drax", 4),
-        ("Mantis", 3),
-        ("Nebula", 4),
-        ("Shang-Chi", 1),
-        ("Eternals", 1),
-        ("Wong", 5),
-        ("Happy Hogan", 4),
-        ("Pepper Potts", 4),
-        ("Red Skull", 2),
-        ("Thanos", 4),
-        ("Ultron", 2),
-        ("Hela", 1),
-        ("Yondu", 2)
+        ("Rocket Raccoon", 6), ("Groot", 5), ("Iron Man", 10), ("Captain America", 9),
+        ("Black Widow", 8), ("Thor", 8), ("Hulk", 6), ("Doctor Strange", 5),
+        ("Spider-Man", 5), ("Black Panther", 4), ("Star-Lord", 5), ("Gamora", 4),
+        ("Drax", 4), ("Captain Marvel", 3), ("Scarlet Witch", 5), ("Vision", 4),
+        ("Falcon", 5), ("Winter Soldier", 5), ("War Machine", 7), ("Loki", 6),
+        ("Nick Fury", 9), ("Hawkeye", 5), ("Ant-Man", 4), ("Wasp", 3),
+        ("Doctor Strange", 5), ("Star-Lord", 5), ("Gamora", 4), ("Drax", 4),
+        ("Mantis", 3), ("Nebula", 4), ("Shang-Chi", 1), ("Eternals", 1), ("Wong", 5),
+        ("Happy Hogan", 4), ("Pepper Potts", 4), ("Red Skull", 2), ("Thanos", 4),
+        ("Ultron", 2), ("Hela", 1), ("Yondu", 2)
     ]
-    
-    for nombre, peliculas in personajes:
-        pila.apilar(Personaje(nombre, peliculas))
-    
+    for i in range(len(personajes)):
+        nombre, cantidad = personajes[i]
+        pila.push(Personaje(nombre, cantidad))
     return pila
 
+
 def posicion_rocket_groot(pila):
+    aux = Stack()
     pos_rocket = -1
     pos_groot = -1
-    posicion = 1  
-    
-    pila_temp = Pila()
-    
-    while not pila.esta_vacia():
-        personaje = pila.desapilar()
-        pila_temp.apilar(personaje)
-        
-        if personaje.nombre == "Rocket Raccoon":
+    posicion = 1
+    while not pila.is_empty():
+        p = pila.pop()
+        aux.push(p)
+        if p.nombre == "Rocket Raccoon":
             pos_rocket = posicion
-        if personaje.nombre == "Groot":
+        if p.nombre == "Groot":
             pos_groot = posicion
-        
         posicion += 1
-    
-    
-    while not pila_temp.esta_vacia():
-        pila.apilar(pila_temp.desapilar())
-    
+    while not aux.is_empty():
+        pila.push(aux.pop())
     return (pos_rocket, pos_groot)
 
-def personajes_mas_de_5_peliculas(pila):
+
+def personajes_mas_de_5(pila):
+    aux = Stack()
     resultados = []
-    pila_temp = Pila()
-    
-    while not pila.esta_vacia():
-        personaje = pila.desapilar()
-        pila_temp.apilar(personaje)
-        
-        if personaje.peliculas > 5:
-            resultados.append((personaje.nombre, personaje.peliculas))
-    
-    
-    while not pila_temp.esta_vacia():
-        pila.apilar(pila_temp.desapilar())
-    
+    while not pila.is_empty():
+        p = pila.pop()
+        if p.peliculas > 5:
+            resultados.append(p)
+        aux.push(p)
+    while not aux.is_empty():
+        pila.push(aux.pop())
     return resultados
 
+
 def peliculas_viuda_negra(pila):
-    peliculas = 0
-    pila_temp = Pila()
-    
-    while not pila.esta_vacia():
-        personaje = pila.desapilar()
-        pila_temp.apilar(personaje)
-        
-        if personaje.nombre == "Black Widow":
-            peliculas = personaje.peliculas
-            break
-    
-    
-    while not pila_temp.esta_vacia():
-        pila.apilar(pila_temp.desapilar())
-    
-    return peliculas
+    aux = Stack()
+    resultado = 0
+    encontrado = False
+    while not pila.is_empty():
+        p = pila.pop()
+        if not encontrado and p.nombre == "Black Widow":
+            resultado = p.peliculas
+            encontrado = True
+        aux.push(p)
+    while not aux.is_empty():
+        pila.push(aux.pop())
+    return resultado
+
 
 def personajes_letras_cdg(pila):
+    aux = Stack()
     resultados = []
-    pila_temp = Pila()
-    
-    while not pila.esta_vacia():
-        personaje = pila.desapilar()
-        pila_temp.apilar(personaje)
-        
-        primera_letra = personaje.nombre[0].upper()
-        if primera_letra in ['C', 'D', 'G']:
-            resultados.append(personaje)
-    
-    
-    while not pila_temp.esta_vacia():
-        pila.apilar(pila_temp.desapilar())
-    
+    while not pila.is_empty():
+        p = pila.pop()
+        letra = p.nombre[0].upper()
+        if letra == "C" or letra == "D" or letra == "G":
+            resultados.append(p)
+        aux.push(p)
+    while not aux.is_empty():
+        pila.push(aux.pop())
     return resultados
 
 
 if __name__ == "__main__":
     pila_mcu = crear_pila_mcu()
-    
-  
-    pos_rocket, pos_groot = posicion_rocket_groot(pila_mcu)
-    print("\na. Posiciones de Rocket Raccoon y Groot:")
-    print(f"Rocket Raccoon está en la posición: {pos_rocket}")
-    print(f"Groot está en la posición: {pos_groot}")
-    
-   
-    mas_de_5 = personajes_mas_de_5_peliculas(pila_mcu)
-    print("\nb. Personajes que participaron en más de 5 películas:")
-    for nombre, peliculas in mas_de_5:
-        print(f"{nombre}: {peliculas} películas")
-    
-    
-    peliculas_bw = peliculas_viuda_negra(pila_mcu)
-    print("\nc. Películas en las que participó Black Widow:")
-    print(f"Black Widow participó en {peliculas_bw} películas")
-    
-    
-    personajes_cdg = personajes_letras_cdg(pila_mcu)
-    print("\nd. Personajes cuyos nombres empiezan con C, D y G:")
-    for personaje in personajes_cdg:
-        print(personaje)
+
+    print("a. Posiciones de Rocket Raccoon y Groot:")
+    pos_r, pos_g = posicion_rocket_groot(pila_mcu)
+    print("Rocket Raccoon está en la posición:", pos_r)
+    print("Groot está en la posición:", pos_g)
+
+    print("b. Personajes con más de 5 películas:")
+    mas_de_5 = personajes_mas_de_5(pila_mcu)
+    for i in range(len(mas_de_5)):
+        mostrar_personaje(mas_de_5[i])
+
+    print("c. Películas en las que participó Black Widow:")
+    cantidad = peliculas_viuda_negra(pila_mcu)
+    print("Black Widow participó en", cantidad, "películas")
+
+    print("d. Personajes cuyos nombres empiezan con C, D o G:")
+    seleccionados = personajes_letras_cdg(pila_mcu)
+    for i in range(len(seleccionados)):
+        mostrar_personaje(seleccionados[i])
